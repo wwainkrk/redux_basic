f2cab2b4a434a7e58f02087eb46a802ab7b4f2bc
Sebastian Warszawa <warszawa@fev.com>
2020-03-12 18:37:27 +0100

    First commit, import store method from redux, simple reducer, and store variable for easily access to object

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialMovies = {
+  movies: [
+    'Prestige', 'Wolf of Wall Street', '60 seconds'
+  ]
+}
+
+function movies(state = initialMovies, action) {
+  switch (action.type) {
+    case 'RESET':
+      return {
+        movies: []
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(movies)
+window.store = store
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
