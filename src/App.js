78e86c3779d8d17433f1b3d793da23980dd2c800
Warszawa <warszawa@fev.com>
2020-03-17 11:48:41 +0100

    Comments, install redux-devtools, we keep old data in reducer

--- src/App.js
+++ src/App.js
@@ -2,27 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
+/* Initial data for feature */
 const initialMovies = {
+  listName: 'Favourite',
   movies: [
     'Prestige', 'Wolf of Wall Street', '60 seconds'
   ]
 }
 
+
+/* Basic reducer with few action for dispatch */
 function movies(state = initialMovies, action) {
   switch (action.type) {
-    case 'RESET':
+    case 'RESET':                                         // first action
       return {
-        movies: []
+        ...state, movies: []                              // we keep old data in 'state'
+      }
+    case 'ADD':                                           // second action  
+      return {
+        ...state, movies: [...state.movies, action.movie] // we keep old data in 'state', movie will be provided as argument
       }
     default:
       return state
   }
 }
 
-const store = createStore(movies)
+const store = createStore(movies, composeWithDevTools())    // initiate Redux store
 window.store = store
 
+
+/* Main component to render - simpy react-app */
 class App extends Component {
   render() {
     return (
